bb11c618c8c2a1a1a2e5a9467317dbefbe1dfac9
Cristian  Camilo Soto <crcsotop@gmail.com>
2025-02-25 19:53:47 -0500

    Creacion de funcion sortearAnigo

--- app.js
+++ app.js
@@ -23,8 +23,23 @@ function mostrarAmigo()
     nombreAmigo.innerHTML = ""; // limpiamos la lista antes de actualizar para asegurarnos que no haya duplicados
     for (let i=0; i < amigos.length; i++)
     {
-        let lista = document.createElement("li");// creamos el elemento li 
+        let lista = document.createElement("li"); // creamos el elemento li 
         lista.textContent = amigos[i];//Estamos agregando el texto que se encuentra dentro de la lista amigos en su parte i
         nombreAmigo.appendChild(lista);//Agregamos los elementos li ya con su contenido
     }
+}
+
+function sortearAmigo()
+{
+    if (amigos.length == 0)
+    {
+        agregarAmigo();
+    }else{
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        //alert(`Nombre seleccionado ${amigos[indiceAleatorio]}`);
+        let nombreAleatorio = amigos[indiceAleatorio];
+        let resultadoAmigo = document.getElementById ("resultado")
+        resultadoAmigo.innerHTML = `El amigo sorteado es: ${nombreAleatorio}`;
+    }
+
 }
